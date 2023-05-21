ddc30e0223fbef61d49f8ee9387e59259a6b4844
femellone <38559436+femellone@users.noreply.github.com>
2023-05-21 03:20:23 -0400

    fixes

--- src/builders/modules.js
+++ src/builders/modules.js
@@ -8,62 +8,59 @@ const createModule = (moduleName, callback) => {
   const moduleNameFirstLetterUpper = firstLetter + moduleNameLowerCase.slice(1)
   const files = [
     {
-      name: `
-            src/modules/${moduleNameLowerCase}/data/data_source/pg_data_source.ts`,
+      name: `src/modules/${moduleNameLowerCase}/data/data_source/pg_data_source.ts`,
       content: `import { Pool, PoolClient, QueryResult } from "pg";
-            import {${moduleNameFirstLetterUpper}DataSource} from "../interfaces/${moduleNameLowerCase}_data_source";
+import {${moduleNameFirstLetterUpper}DataSource} from "../interfaces/${moduleNameLowerCase}_data_source";
 
-            export class PG${moduleNameFirstLetterUpper}sDataSource implements ${moduleNameFirstLetterUpper}DataSource {
-              private db: Pool;
-              private constructor(db: Pool) {
-                this.db = db;
-              }
-              static instance: PG${moduleNameFirstLetterUpper}sDataSource | null = null;
+export class PG${moduleNameFirstLetterUpper}sDataSource implements ${moduleNameFirstLetterUpper}DataSource {
+  private db: Pool;
+  private constructor(db: Pool) {
+    this.db = db;
+  }
+  static instance: PG${moduleNameFirstLetterUpper}sDataSource | null = null;
 
-              static create(dataSource: Pool) {
-                if (PG${moduleNameFirstLetterUpper}sDataSource.instance == null) {
-                  PG${moduleNameFirstLetterUpper}sDataSource.instance = new PG${moduleNameFirstLetterUpper}sDataSource(dataSource);
-                }
-                return PG${moduleNameFirstLetterUpper}sDataSource.instance;
-              }
+  static create(dataSource: Pool) {
+    if (PG${moduleNameFirstLetterUpper}sDataSource.instance == null) {
+      PG${moduleNameFirstLetterUpper}sDataSource.instance = new PG${moduleNameFirstLetterUpper}sDataSource(dataSource);
+    }
+    return PG${moduleNameFirstLetterUpper}sDataSource.instance;
+  }
 
-              // GENERIC FUNCTION
-              private async callDataBase<T>(
-                query: string,
-                values: any[],
-                callback: (result: QueryResult<any>) => T
-              ): Promise<T> {
-                let client: PoolClient;
-                client = await this.db.connect();
-                try {
-                  const response = await client.query(query, values);
-                  return callback(response);
-                } catch (err) {
-                  if (err instanceof CustomError) {
-                    throw err;
-                  }
-                  throw new DataBaseError(err as Error);
-                } finally {
-                  if (client) {
-                    client.release();
-                  }
-                }
-              }
-            }`,
+  // GENERIC FUNCTION
+  private async callDataBase<T>(
+    query: string,
+    values: any[],
+    callback: (result: QueryResult<any>) => T
+  ): Promise<T> {
+    let client: PoolClient;
+    client = await this.db.connect();
+    try {
+      const response = await client.query(query, values);
+      return callback(response);
+    } catch (err) {
+      if (err instanceof CustomError) {
+        throw err;
+      }
+      throw new DataBaseError(err as Error);
+    } finally {
+      if (client) {
+        client.release();
+      }
+    }
+  }
+}`,
     },
     {
       name: `src/modules/${moduleNameLowerCase}/data/interfaces/${moduleNameLowerCase}_data_source.ts`,
-      content: `
-            export interface ${moduleNameFirstLetterUpper}DataSource {
-              // IMPLEMENT METHODS
-            }`,
+      content: `export interface ${moduleNameFirstLetterUpper}DataSource {
+  // IMPLEMENT METHODS
+}`,
     },
     {
       name: `src/modules/${moduleNameLowerCase}/domain/models/${moduleNameLowerCase}_model.ts`,
-      content: `
-            export interface ${moduleNameFirstLetterUpper} {};
-            export interface Create${moduleNameFirstLetterUpper} extends Omit<${moduleNameFirstLetterUpper}, ""> {};
-            export interface Update${moduleNameFirstLetterUpper} extends Partial<${moduleNameFirstLetterUpper}> {}`,
+      content: `export interface ${moduleNameFirstLetterUpper} {};
+  export interface Create${moduleNameFirstLetterUpper} extends Omit<${moduleNameFirstLetterUpper}, ""> {};
+  export interface Update${moduleNameFirstLetterUpper} extends Partial<${moduleNameFirstLetterUpper}> {}`,
     },
     {
       name: `src/modules/${moduleNameLowerCase}/domain/repositories/${moduleNameLowerCase}_repository.ts`,
@@ -71,37 +68,36 @@ const createModule = (moduleName, callback) => {
     },
     {
       name: `src/modules/${moduleNameLowerCase}/domain/repositories/${moduleNameLowerCase}_repository_implementation.ts`,
-      content: `
-            import { ${moduleNameFirstLetterUpper}Repository } from "./${moduleNameLowerCase}_repository";
-            import { ${moduleNameFirstLetterUpper}DataSource } from "../../data/interfaces/${moduleNameLowerCase}_data_source";
+      content: `import { ${moduleNameFirstLetterUpper}Repository } from "./${moduleNameLowerCase}_repository";
+import { ${moduleNameFirstLetterUpper}DataSource } from "../../data/interfaces/${moduleNameLowerCase}_data_source";
 
-            export class ${moduleNameFirstLetterUpper}RepositoryImplementation implements ${moduleNameFirstLetterUpper}Repository{
-              private ${moduleNameLowerCase}sDataSource: ${moduleNameFirstLetterUpper}DataSource;
-              private constructor(dataSource: ${moduleNameLowerCase}DataSource) {
-                this.usersDataSource = dataSource;
-              }
+export class ${moduleNameFirstLetterUpper}RepositoryImplementation implements ${moduleNameFirstLetterUpper}Repository{
+  private ${moduleNameLowerCase}sDataSource: ${moduleNameFirstLetterUpper}DataSource;
+  private constructor(dataSource: ${moduleNameLowerCase}DataSource) {
+    this.usersDataSource = dataSource;
+  }
 
-              static instance: ${moduleNameFirstLetterUpper}RepositoryImplementation | null = null;
+  static instance: ${moduleNameFirstLetterUpper}RepositoryImplementation | null = null;
 
-              static create(dataSource: ${moduleNameFirstLetterUpper}DataSource) {
-                if (${moduleNameFirstLetterUpper}RepositoryImplementation.instance == null) {
-                  ${moduleNameFirstLetterUpper}RepositoryImplementation.instance = new ${moduleNameFirstLetterUpper}RepositoryImplementation(dataSource);
-                }
-                return ${moduleNameFirstLetterUpper}RepositoryImplementation.instance;
-              }
+  static create(dataSource: ${moduleNameFirstLetterUpper}DataSource) {
+    if (${moduleNameFirstLetterUpper}RepositoryImplementation.instance == null) {
+      ${moduleNameFirstLetterUpper}RepositoryImplementation.instance = new ${moduleNameFirstLetterUpper}RepositoryImplementation(dataSource);
+    }
+    return ${moduleNameFirstLetterUpper}RepositoryImplementation.instance;
+  }
 
-              private async callDataSource<T>(callback: Function): Promise<T> {
-                try {
-                  return await callback();
-                } catch (error) {
-                  console.log(error);
-                  if (error instanceof CustomError) {
-                    throw error;
-                  }
-                  throw new GenericError("Users Repositories error.");
-                }
-              }
-            }`,
+  private async callDataSource<T>(callback: Function): Promise<T> {
+    try {
+      return await callback();
+    } catch (error) {
+      console.log(error);
+      if (error instanceof CustomError) {
+        throw error;
+      }
+      throw new GenericError("Users Repositories error.");
+    }
+  }
+  }`,
     },
     {
       name: `src/modules/${moduleNameLowerCase}/presentation/${moduleNameLowerCase}_router.ts`,
